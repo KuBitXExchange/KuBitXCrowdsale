478cffa7ea24ab38007436fd23ca03845e863a25
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2018-11-12 01:12:08 +0530

    Initial Commit

---
+++ contracts/CustomWhitelist.sol
@@ -0,0 +1,81 @@
+/*
+Copyright 2018 Binod Nirvan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+pragma solidity 0.4.24;
+import "./CustomPausable.sol";
+
+///@title This contract enables to maintain a list of whitelisted wallets.
+contract CustomWhitelist is CustomPausable {
+  mapping(address => bool) public whitelist;
+
+  event WhitelistAdded(address indexed _account);
+  event WhitelistRemoved(address indexed _account);
+
+  ///@notice Verifies if the account is whitelisted.
+  modifier ifWhitelisted(address _account) {
+    require(_account != address(0));
+    require(whitelist[_account]);
+
+    _;
+  }
+
+  ///@notice Adds an account to the whitelist.
+  ///@param _account The wallet address to add to the whitelist.
+  function addWhitelist(address _account) external whenNotPaused onlyAdmin {
+    require(_account!=address(0));
+
+    if(!whitelist[_account]) {
+      whitelist[_account] = true;
+
+      emit WhitelistAdded(_account);
+    }
+  }
+
+  ///@notice Adds multiple accounts to the whitelist.
+  ///@param _accounts The wallet addresses to add to the whitelist.
+  function addManyWhitelist(address[] _accounts) external whenNotPaused onlyAdmin {
+    for(uint8 i=0;i<_accounts.length;i++) {
+      if(_accounts[i] != address(0) && !whitelist[_accounts[i]]) {
+        whitelist[_accounts[i]] = true;
+
+        emit WhitelistAdded(_accounts[i]);
+      }
+    }
+  }
+
+  ///@notice Removes an account from the whitelist.
+  ///@param _account The wallet address to remove from the whitelist.
+  function removeWhitelist(address _account) external whenNotPaused onlyAdmin {
+    require(_account != address(0));
+    if(whitelist[_account]) {
+      whitelist[_account] = false;
+
+      emit WhitelistRemoved(_account);
+    }
+  }
+
+  ///@notice Removes multiple accounts from the whitelist.
+  ///@param _accounts The wallet addresses to remove from the whitelist.
+  function removeManyWhitelist(address[] _accounts) external whenNotPaused onlyAdmin {
+    for(uint8 i=0;i<_accounts.length;i++) {
+      if(_accounts[i] != address(0) && whitelist[_accounts[i]]) {
+        whitelist[_accounts[i]] = false;
+
+        emit WhitelistRemoved(_accounts[i]);
+      }
+    }
+  }
+}
---
+++ contracts/TokenSale.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.4.22;
+import "openzeppelin-solidity/contracts/crowdsale/distribution/FinalizableCrowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "./CustomWhitelist.sol";
+
+contract TokenSale is CappedCrowdsale, FinalizableCrowdsale, CustomWhitelist {
+  using SafeMath for uint256;
+
+  event FundsWithdrawn(address indexed _wallet, uint256 _amount);
+  event BonusChanged(uint256 _newBonus, uint256 _oldBonus);
+
+  uint256 public bonus;
+
+  constructor(uint256 _openingTime, uint256 _closingTime, uint256 _rate, address _wallet, IERC20 _token, uint256 _bonus, uint256 _cap)
+  TimedCrowdsale(_openingTime, _closingTime) CappedCrowdsale(_cap) Crowdsale(_rate, _wallet, _token) public {
+    require(_bonus > 0);
+    bonus = _bonus;
+  }
+
+
+  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal view whenNotPaused ifWhitelisted(_beneficiary) {
+   super._preValidatePurchase(_beneficiary, _weiAmount);
+ }
+
+
+ function _getTokenAmount(uint256 weiAmount) internal view returns (uint256) {
+   uint256 tokenAmount = super._getTokenAmount(weiAmount);
+   uint256 bonusTokens = tokenAmount.mul(bonus).div(100);
+   return tokenAmount.add(bonusTokens);
+ }
+
+
+  function _forwardFunds() internal {
+
+  }
+
+
+  function withdrawFunds(uint256 _amount) external whenNotPaused onlyAdmin {
+    require(_amount <= address(this).balance);
+    msg.sender.transfer(_amount);
+    emit FundsWithdrawn(msg.sender, _amount);
+  }
+
+  function withdrawTokens() public whenNotPaused onlyAdmin {
+    IERC20 t = super.token();
+    t.safeTransfer(msg.sender, t.balanceOf(this));
+  }
+
+  function changeBonus(uint256 _bonus) external whenNotPaused onlyAdmin {
+    require(_bonus > 0);
+    emit BonusChanged(_bonus, bonus);
+    bonus = _bonus;
+  }
+
+  function hasClosed() public view returns (bool) {
+    return super.hasClosed() || super.capReached();
+  }
+
+
+
+}
